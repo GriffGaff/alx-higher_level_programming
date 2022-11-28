9f557e2ae656854ed9c8335cda0814dc2a5251fe
GriffGaff <bolajibaqi@guser.email>
2022-11-28 15:18:36 -0800

    4th basic python task

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = print(word[:2])
+word_last_2 = print(word[6:])
+middle_word = print(word[0:-1])
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
